e676ce9b96c602a192181067d6ff0510091f91c1
arifbaira7864-cloud <arifbaira786.4@gmail.com>
2026-05-25 09:47:26 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+console.log("Velora Fully Working App Loaded");
+
+// LIKE SYSTEM
+function likeVideo(){
+  alert("❤️ Liked!");
+}
+
+// COMMENT SYSTEM
+function commentVideo(){
+  alert("💬 Comment panel open (next step we build UI)");
+}
+
+// SHARE SYSTEM
+function shareVideo(){
+  alert("🔗 Shared!");
+}
+
+// FOLLOW SYSTEM
+function followUser(){
+  alert("➕ Followed!");
+}
+
+// NAVIGATION
+function goHome(){
+  alert("Home Page");
+}
+
+function goUpload(){
+  alert("Upload Page");
+}
+
+function goInbox(){
+  alert("Inbox Page");
+}
+
+function goProfile(){
+  alert("Profile Page");
+}
