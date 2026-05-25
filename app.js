6c4d46ee9d863b271444b0d118f5dc62f7c2fc78
arifbaira7864-cloud <arifbaira786.4@gmail.com>
2026-05-25 10:11:26 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+function goHome(){ window.location.href="home.html"; }
+function goUpload(){ window.location.href="upload.html"; }
+function goInbox(){ window.location.href="inbox.html"; }
+function goProfile(){ window.location.href="profile.html"; }
+
+function like(){ alert("❤️ Liked"); }
+function comment(){ alert("💬 Comment open"); }
+function share(){ alert("🔗 Shared"); }
+function follow(){ alert("➕ Followed"); }
